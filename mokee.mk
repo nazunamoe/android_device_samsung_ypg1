f774a1e4df3cf2c52d6e4454f545d69d1b29e885
nazunamoe <jpg3927@gmail.com>
2013-08-08 11:47:28 +0900

    Update mokee.mk

--- mokee.mk
+++ mokee.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/samsung/ypg1/full_ypg1.mk)
 
 ## Device identifiEr. This must come after all inclusions
 PRODUCT_DEVICE := ypg1
-PRODUCT_NAME := cm_ypg1
+PRODUCT_NAME := mk_ypg1
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := YP-G1
 PROUDCT_MANUFACTURER := Samsung
